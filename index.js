031dbcdf13cd9ba0e5f9fab7648c1227976ef815
alexalexalex101 <alexro3258@gmail.com>
2026-01-08 22:35:50 -0500

    created report page, no data functionality yet

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById("reportbutton").onclick = function () {
+        location.href = "report.html";
+    };
+
+document.getElementById("findbutton").onclick = function () {
+        location.href = "find.html";
+    };    
